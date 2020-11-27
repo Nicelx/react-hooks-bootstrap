61a7a138d14ca58d8040a623b19880522ff69ab2
Nicelx <Relush321@gmail.com>
2020-11-27 22:55:44 +0300

    some magic

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -5,10 +5,10 @@ import { useReducer, useEffect } from "react"
 const ACTIONS = {
 	MAKE_REQUEST : 'make-request',
 	GET_DATA : 'get-data',
-	ERROE : 'error'
+	ERROR : 'error'
 }
 
-const BASE_URL = 'https://jobs.github.com/positions.json?description=python&full_time=true&location=sf'
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
 
 function reducer(state, action) {
 	switch (action.type) {
@@ -17,7 +17,7 @@ function reducer(state, action) {
 		case ACTIONS.GET_DATA : 
 			return { ...state, loading : false, jobs: action.payload.jobs}
 		case ACTIONS.ERROR : 
-			return {... state, loading: false, error: action.payload.error, jobs: []}
+			return {...state, loading: false, error: action.payload.error, jobs: []}
 		default : 
 			return state
 	}
@@ -27,18 +27,22 @@ export default function useFetchJobs(params, page) {
 	const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true})
 
 	useEffect(() => {
+		const cancelToken = axios.CancelToken.source()
 		dispatch({type: ACTIONS.MAKE_REQUEST})
 		axios.get(BASE_URL, {
+			cancelToken: cancelToken.token, 
 			params: {markdown: true, page: page, ...params}
 		}).then(res => {
 			dispatch({type: ACTIONS.GET_DATA, payload: {jobs: res.data}})
+		}).catch(e => {
+			if (axios.isCancel(e)) return 
+			dispatch({type: ACTIONS.ERROR, payload: {error: e}})
 		})
+
+		return () => {
+			cancelToken.cancel()			
+		}
 	}, [params, page])
 	
-	return {
-		jobs : [],
-		loading : true,
-		error : false
-
-	}
+	return state;
 }
