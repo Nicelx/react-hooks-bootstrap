243d3ae008dd9682b431b36ad7a92c57a5199fe4
Nicelx <Relush321@gmail.com>
2020-12-01 23:38:11 +0300

    card complete

---
+++ src/JobsPagination.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Pagination } from 'react-bootstrap';
+
+function JobsPagination({page, setPage}) {
+	return (
+		<div>
+			
+		</div>
+	);
+}
+
+export default JobsPagination;
